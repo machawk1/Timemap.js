3f7d3e9711cf6243102679310a81bafac555e86b
Mat Kelly <machawk1@gmail.com>
2014-01-13 11:18:02 -0500

    Fix compilation/interpretation issues with js including some index lookup porting. Added test html file.

--- timemap.js
+++ timemap.js
@@ -14,10 +14,10 @@ import urllib2*/
 // REGULAR EXPRESSIONS
 //==================================================================
 
-tokenizer = re.compile('(<[^>]+>|[a-zA-Z]+="[^"]*"|[;,])\\s*');
+tokenizer = new RegExp('(<[^>]+>|[a-zA-Z]+="[^"]*"|[;,])\\s*'); 
 
 //==================================================================
-function TimeMap(object,timemap_uri):
+function TimeMap(object,timemap_uri){
 //==================================================================
 
     //def __init__(self, timemap_uri):
@@ -34,20 +34,20 @@ function TimeMap(object,timemap_uri):
 		if(link[0] == 'memento')
 			this.mementos[link[1]] = link[2];
 		else if(link[0] == 'original')
-			this.original = link ? link[2] : else null;
+			this.original = link ? link[2] : null;
 		else if(link[0] == 'timebundle')
-			this.timebundle = link ? link[2] : else null;
+			this.timebundle = link ? link[2] : null;
 		else if(link[0] == 'timegate')
-			this.timegate = link ? link[2] : else null;
+			this.timegate = link ? link[2] : null;
 		else if(link[0] == 'timemap')
-			this.timemap = link ? link[2] : else null;
-		else if(link[0] == 'first memento')
+			this.timemap = link ? link[2] : null;
+		else if(link[0] == 'first memento'){
 			this.mementos[link[1]] = link[2]
-			this.first_memento = link ? link[1] : else null;
-		else if(link[0] == 'last memento')){
+			this.first_memento = link ? link[1] : null;}
+		else if(link[0] == 'last memento'){
 			this.mementos[link[1]] = link[2];
-			this.last_memento = link ? link[1] : else null;}
-		link = this.get_next_link()
+			this.last_memento = link ? link[1] : null;}
+		link = this.get_next_link();
 	}
     function get_next_link(){
         uri = null;
@@ -56,21 +56,21 @@ function TimeMap(object,timemap_uri):
         resource_type = null;
         for(token in this.__tokens){
             if(token[0] == '<')
-                uri = token[1:-1];
-            else if(token[:9] == 'datetime=')
-                datetime = token[10:-1];
-            else if(token[:4] == 'rel=')
-                rel = token[5:-1];
-            else if(token[:5] == 'type=')
-                resource_type = token[6:-1];
+                uri = token.slice[1,token.length-2]; //token[1:-1] TO VERIFY: is this getting everything between the first and last element exclusive? 
+            else if(token.substr(0,9) == 'datetime=') //token[:9] == 'datetime='
+                datetime = token.substr(10);//token[10:-1];
+            else if(token.substr(0,4) == 'rel=')
+                rel = token.substr(5);
+            else if(token.substr(0,5) == 'type=')
+                resource_type = token.substr(6);
             else if(token == ';')
                 null;
             else if(token == ',')
                 return ( rel, (datetime ? 
                               dateutil.parser.parse(datetime) : null),
                               uri, resource_type )
-            else:
-                raise Exception('Unexpected timemap token', token) }
+            else
+                throw {name:'Unexpected timemap token',token: token}   }//end for
         if(!uri)
             return null
         else
@@ -80,26 +80,26 @@ function TimeMap(object,timemap_uri):
 	}
     function __getitem__(key){
         return this.mementos[key];}
-
+}
 //==================================================================
-function TimeMapTokenizer(object):
+function TimeMapTokenizer(object){
 //==================================================================
 
     //def __init__(self, timemap_uri):
-    this._tmfile = urllib2.urlopen(timemap_uri);
+    var xmlhttp = new XMLHttpRequest(); xmlhttp.open("GET",timemap_uri,false); xmlhttp.send(); this._tmfile = xmlhttp.responseText;
     this._tokens = [];
 
     function __iter__(){
         return this;}
 
     function next(){
-        if this._tokens.length == 0{
+        if(this._tokens.length == 0){
             line = this._tmfile.readline();
             if(line.length == 0)
-                raise StopIteration;
+                throw {name: "StopIteration"};
             this._tokens = tokenizer.findall(line);} //end conditional
         return this._tokens.pop(0);} //end next function
-
+}
 //===============================================================================
 // MAIN FOR TESTING
 //===============================================================================
@@ -108,15 +108,15 @@ function TimeMapTokenizer(object):
     //import logging
     //logging.basicConfig()
 
-    timemap_uri = "http://api.wayback.archive.org/memento/timemap/link/http://www.cs.odu.edu";
-    tm = TimeMap(timemap_uri);
-    console.log("Original:     "+tm.original);
-    console.log("Time Bundle:  ", tm.timebundle);
-    console.log("Time Gate:    ", tm.timegate);
-    console.log("Time Map:     ", tm.timemap);
-    console.log("First Memento:", tm.first_memento);
-    console.log("Last Memento: ", tm.last_memento);
-    console.log("Mementos:");
-    for(memento in sorted(tm.mementos.keys())){
-        console.log(memento, "=", tm.mementos[memento]);}
+timemap_uri = "http://api.wayback.archive.org/memento/timemap/link/http://www.cs.odu.edu";
+tm = TimeMap(timemap_uri);
+console.log("Original:     "+tm.original);
+console.log("Time Bundle:  ", tm.timebundle);
+console.log("Time Gate:    ", tm.timegate);
+console.log("Time Map:     ", tm.timemap);
+console.log("First Memento:", tm.first_memento);
+console.log("Last Memento: ", tm.last_memento);
+console.log("Mementos:");
+for(memento in sorted(tm.mementos.keys())){
+	console.log(memento, "=", tm.mementos[memento]);}
 
